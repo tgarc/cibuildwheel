0d740805e71fd1d685260522ef573eae3ab18c8f
tdos.apone <toemossgarcia@gmail.com>
2017-06-04 22:36:48 -0500

    expand variables in CIBW_ENVIRONMENT

--- cibuildwheel/linux.py
+++ cibuildwheel/linux.py
@@ -103,7 +103,7 @@ def build(project_dir, package_name, output_dir, test_command, test_requires, be
                 '-i',
                 '-v', '%s:/project' % os.path.abspath(project_dir),
                 '-v', '%s:/output' % os.path.abspath(output_dir)]
-                + [ '-e %s' % kv for kv in os.environ.get('CIBW_ENVIRONMENT', '').split() ]
+                + [ '-e %s' % os.path.expandvars(kv) for kv in os.environ.get('CIBW_ENVIRONMENT', '').split() ]
                 + [ docker_image,
                 '/bin/bash'],
             stdin=subprocess.PIPE, universal_newlines=True)
